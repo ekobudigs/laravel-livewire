0b2faae84da17166c7b878983df532b73662b381
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-10-13 16:39:55 +0700

    Create Livewire User

---
+++ app/Livewire/Users/Show.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Users;
+
+use Livewire\Component;
+
+class Show extends Component
+{
+    public function render()
+    {
+        return view('livewire.users.show');
+    }
+}
