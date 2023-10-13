3b20ab85f743f174c1a40922748f54b262bba4b4
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-10-13 17:01:54 +0700

    update seed and route users

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Livewire\About;
 use App\Livewire\Contact;
 use App\Livewire\Home;
 use App\Livewire\Posts\Index;
+use App\Livewire\Users\Show;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,3 +23,6 @@ Route::get('/', Home::class)->name('home');
 Route::get('/about', About::class)->name('about');
 Route::get('/contact', Contact::class)->name('contact');
 Route::get('/posts', Index::class)->name('posts.index');
+
+
+Route::get('/users/{user}', Show::class)->name('users.name');
