3acbf88d934c171ba3eb44167718df7034186732
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-10-16 10:56:03 +0700

    update add nav link component

---
+++ resources/views/components/navigation-menu.blade.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/">
+            {{ config('app.name') }}
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+                <x-nav-link :active="request()->routeIs('home')" href="/">Home </x-nav-link>
+                <x-nav-link :active="request()->routeIs('about')" href="/about">About </x-nav-link>
+                <x-nav-link :active="request()->routeIs('contact')" href="/contact">Contact </x-nav-link>
+                <x-nav-link :active="request()->routeIs('posts.*')" href="/posts">Posts </x-nav-link>
+
+
+            </ul>
+
+        </div>
+    </div>
+</nav>
