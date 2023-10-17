e6d383de6de262a03e64489e42f913a7cb740ce5
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-10-17 14:32:14 +0700

    refactor blade

--- resources/views/livewire/posts/create.blade.php
+++ resources/views/livewire/posts/create.blade.php
@@ -1,36 +1,45 @@
-<div class="card">
+<div>
 
-    <div class="card-body">
-        <div class="card-body">
-            <h5 class="card-title">
-                New Post
-            </h5>
-
-            <form wire:submit="save">
-                <div class="mb-4">
-                    <label for="title" class="form-label">Title</label>
-                    <input type="text" wire:model="form.title" class="form-control" id="title" />
-
-                    @error('form.title')
-                        <small class="text-danger d-block mt-1"> {{ $message }} </small>
-                    @enderror
-                </div>
-
-
-                <div class="mb-4">
-                    <label for="body" class="form-label">body</label>
-                    <textarea type="text" wire:model="form.body" class="form-control" id="body"> </textarea>
-                    @error('form.body')
-                        <small class="text-danger d-block mt-1"> {{ $message }} </small>
-                    @enderror
-                </div>
-
-                <button class="btn-primary">
-                    Save
-                </button>
-            </form>
+    @if (flash()->message)
+        <div class="alert alert-{{ flash()->class }}" role="alert">
+            {{ flash()->message }}
         </div>
-    </div>
+    @endif
+    <div class="card">
 
+        <div class="card-body">
+            <div class="card-body">
+                <h5 class="card-title">
+                    New Post
+                </h5>
+
+                <form wire:submit="save">
+                    <div class="mb-4">
+                        <label for="title" class="form-label">Title</label>
+                        <input type="text" wire:model="form.title" class="form-control" id="title" />
+
+                        @error('form.title')
+                            <small class="text-danger d-block mt-1"> {{ $message }} </small>
+                        @enderror
+                    </div>
+
+
+                    <div class="mb-4">
+                        <label for="body" class="form-label">body</label>
+                        <textarea type="text" wire:model="form.body" class="form-control" id="body"> </textarea>
+                        @error('form.body')
+                            <small class="text-danger d-block mt-1"> {{ $message }} </small>
+                        @enderror
+                    </div>
+
+                    <button class="btn-primary">
+                        Save
+                    </button>
+                </form>
+            </div>
+        </div>
+
+
+    </div>
 
 </div>
