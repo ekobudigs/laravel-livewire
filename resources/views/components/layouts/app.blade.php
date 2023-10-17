d57ce1fabf9959142ec65faeb73f2bf983742e90
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-10-17 09:17:53 +0700

    add style

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -11,7 +11,11 @@
 
 <body>
     <x-navigation-menu></x-navigation-menu>
-    {{ $slot }}
+    <main class="py-4">
+        <div class="container">
+            {{ $slot }}
+        </div>
+    </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
     </script>
