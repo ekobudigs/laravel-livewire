75adcddf7dd40fec98d7b57953fc4a2d6c5a2a3b
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-10-17 10:11:13 +0700

    add function store

--- app/Livewire/Forms/PostForm.php
+++ app/Livewire/Forms/PostForm.php
@@ -2,10 +2,25 @@
 
 namespace App\Livewire\Forms;
 
-use Livewire\Attributes\Rule;
 use Livewire\Form;
+use App\Models\User;
+use Livewire\Attributes\Rule;
 
 class PostForm extends Form
 {
-    //
+    #[Rule(['required'])]
+    public string $title = '';
+
+    #[Rule(['required'])]
+    public string $body = '';
+
+    public function store()
+    {
+        $user = User::find(1);
+
+
+        $user->posts()->create($this->validate());
+
+        $this->reset();
+    }
 }
