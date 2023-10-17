1cb6376c23e1f77bd5f769e7f9d5c0c7d0e7ad47
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-10-17 09:17:12 +0700

    update add livewire posts

--- resources/views/livewire/posts/index.blade.php
+++ resources/views/livewire/posts/index.blade.php
@@ -1,3 +1,7 @@
 <div>
-    {{-- The best athlete wants his opponent at his best. --}}
+    <div class="row">
+        <div class="col-md-5">
+            <livewire:posts.create />
+        </div>
+    </div>
 </div>
