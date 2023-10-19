85bcda48895f2bad69cd7e871d064ef1a16dc570
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-10-19 16:47:52 +0700

    Create Livewire Login

--- app/Livewire/Login.php
+++ app/Livewire/Login.php
@@ -2,12 +2,32 @@
 
 namespace App\Livewire;
 
-use Livewire\Attributes\Layout;
 use Livewire\Component;
+use Livewire\Attributes\Rule;
+use Livewire\Attributes\Layout;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class Login extends Component
 {
     #[Layout('layouts.guest')]
+
+    #[Rule('required', 'email')]
+    public string $email = '';
+
+    #[Rule('required')]
+    public string $password = '';
+    public function login()
+    {
+
+        if (Auth::attempt($this->validate())) {
+            return redirect()->route('home');
+        }
+
+        throw ValidationException::withMessages([
+            'email' => 'Ine ne provided credentials do not match our records.',
+        ]);
+    }
     public function render()
     {
         return view('livewire.login');
