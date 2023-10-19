8c174b551dc98e8a94194ce66af045e9d7747be9
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-10-19 16:48:29 +0700

    create blade login

---
+++ resources/views/livewire/login.blade.php
@@ -0,0 +1,27 @@
+<div>
+    <div class="card-body">
+        <h5 class="cart-title">
+            Login
+        </h5>
+        <form wire:submit="login">
+            <div class="mb-4">
+                <label for="email" class="form-label">Email</label>
+                <input wire:model="email" class="form-control" type="email" name="email" id="email">
+                @error('email')
+                    <small class="d-block mt-1 text-danger">{{ $message }}</small>
+                @enderror
+            </div>
+
+            <div class="mb-4">
+                <label for="password" class="form-label">password</label>
+                <input wire:model="password" class="form-control" type="password" name="password" id="password">
+                @error('password')
+                    <small class="d-block mt-1 text-danger">{{ $message }}</small>
+                @enderror
+            </div>
+            <button type="submit" class="btn btn-primary">
+                Login
+            </button>
+        </form>
+    </div>
+</div>
