3d5f2873fbeaa07b54d8bd7ab6b6b5420eb33117
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-10-19 15:55:43 +0700

    change route middleware

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
-use App\Livewire\About;
-use App\Livewire\Contact;
 use App\Livewire\Home;
-use App\Livewire\Posts\Index;
+use App\Livewire\About;
+use App\Livewire\Login;
+use App\Livewire\Contact;
 use App\Livewire\Users\Show;
+use App\Livewire\Posts\Index;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NotifyLaravelController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,10 +21,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/', Home::class)->name('home');
-Route::get('/about', About::class)->name('about');
-Route::get('/contact', Contact::class)->name('contact');
-Route::get('/posts', Index::class)->name('posts.index');
+Route::middleware('auth')->group(function () {
+    Route::get('/', Home::class)->name('home');
+    Route::get('/about', About::class)->name('about');
+    Route::get('/contact', Contact::class)->name('contact');
+    Route::get('/posts', Index::class)->name('posts.index');
 
 
-Route::get('/users/{user}', Show::class)->name('users.name');
+    Route::get('/users/{user}', Show::class)->name('users.name');
+});
+
+Route::get('/login', Login::class)->name('login')->middleware('guest');
