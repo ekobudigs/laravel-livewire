f1becbb73b017824f2f9b08d3e82086ae4b8d384
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-10-20 16:43:35 +0700

    Update loginform

--- app/Livewire/Forms/LoginForm.php
+++ app/Livewire/Forms/LoginForm.php
@@ -2,10 +2,29 @@
 
 namespace App\Livewire\Forms;
 
-use Livewire\Attributes\Rule;
 use Livewire\Form;
+use Livewire\Attributes\Rule;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class LoginForm extends Form
 {
-    //
+    #[Rule('required', 'email')]
+    public string $email = '';
+
+    #[Rule('required')]
+    public string $password = '';
+
+    public function store()
+    {
+
+        // dd('a');
+        if (Auth::attempt($this->validate())) {
+            return redirect()->route('home');
+        }
+
+        throw ValidationException::withMessages([
+            'email' => 'Ine ne provided credentials do not match our records.',
+        ]);
+    }
 }
