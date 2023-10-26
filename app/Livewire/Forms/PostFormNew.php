5785513482d8239114ce81a2ca4e41035dae9880
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-10-26 14:55:28 +0700

    Create Post Form New

---
+++ app/Livewire/Forms/PostFormNew.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Livewire\Forms;
+
+use Livewire\Form;
+use Livewire\Attributes\Rule;
+use Illuminate\Support\Facades\Auth;
+
+class PostFormNew extends Form
+{
+
+    #[Rule(['required'])]
+    public string $body = '';
+
+    public function store(): void
+    {
+
+
+
+        Auth::user()->posts()->create($this->validate());
+        flash('Post Created', 'warning');
+        $this->reset();
+    }
+}
