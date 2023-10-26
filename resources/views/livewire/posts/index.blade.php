6254bc7e88439f6de5eedf0468c8a9b882a5ca56
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-10-26 14:56:07 +0700

    Change Use PostNew Livewire

--- resources/views/livewire/posts/index.blade.php
+++ resources/views/livewire/posts/index.blade.php
@@ -1,7 +1,8 @@
 <div>
     <div class="row">
         <div class="col-md-5">
-            <livewire:posts.create />
+            {{-- <livewire:posts.create /> --}}
+            <livewire:posts.post-new />
         </div>
     </div>
 </div>
