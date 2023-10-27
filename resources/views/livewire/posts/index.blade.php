87469697e17955caee1ca97766961ae2fed2c4ba
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-10-27 08:35:17 +0700

    add list post

--- resources/views/livewire/posts/index.blade.php
+++ resources/views/livewire/posts/index.blade.php
@@ -1,8 +1,14 @@
 <div>
     <div class="row">
-        <div class="col-md-5">
+        <div class="col-md-6">
             {{-- <livewire:posts.create /> --}}
             <livewire:posts.post-new />
+        </div>
+
+        <div class="col-md-6">
+            @foreach ($posts as $post)
+                <livewire:posts.block :post="$post" wire:key="{{ $post->id }}" />
+            @endforeach
         </div>
     </div>
 </div>
