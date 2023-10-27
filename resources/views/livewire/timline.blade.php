6eb9afe4bbf4957ac04d12343c861fb4532e5d94
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-10-27 10:53:51 +0700

    update timline blade

---
+++ resources/views/livewire/timline.blade.php
@@ -0,0 +1,14 @@
+<div>
+    <div class="row">
+        <div class="col-md-6">
+            {{--
+            <livewire:posts.create /> --}}
+            <livewire:posts.post-new />
+        </div>
+
+        <div class="col-md-6">
+         
+          <livewire:posts.index lazy />
+        </div>
+    </div>
+</div>
