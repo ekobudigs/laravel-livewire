e48b7496a1326fa1cfc8c99f301954c9ef2871fc
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-10-27 10:53:35 +0700

    refactor  posts index

--- resources/views/livewire/posts/index.blade.php
+++ resources/views/livewire/posts/index.blade.php
@@ -1,14 +1,8 @@
 <div>
-    <div class="row">
-        <div class="col-md-6">
-            {{-- <livewire:posts.create /> --}}
-            <livewire:posts.post-new />
-        </div>
-
-        <div class="col-md-6">
-            @foreach ($posts as $post)
-                <livewire:posts.block :post="$post" wire:key="{{ $post->id }}" />
-            @endforeach
-        </div>
-    </div>
+   
+   @foreach ($posts as $post)
+    <livewire:posts.block :post="$post" wire:key="{{ $post->id }}" />
+    @endforeach
 </div>
+
+
