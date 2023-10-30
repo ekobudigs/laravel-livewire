67b00be1be738f182b09c6cb8f80a5adbfbae941
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-10-30 08:13:06 +0700

    Create Placeholder blade

---
+++ resources/views/livewire/posts/placeholder.blade.php
@@ -0,0 +1,15 @@
+<div>
+<div class="card-body">
+    <h5 class="card-title placeholder-glow">
+        <span class="placeholder col-6"></span>
+    </h5>
+    <p class="card-text placeholder-glow">
+        <span class="placeholder col-7"></span>
+        <span class="placeholder col-4"></span>
+        <span class="placeholder col-4"></span>
+        <span class="placeholder col-6"></span>
+        <span class="placeholder col-8"></span>
+    </p>
+    <a class="btn btn-primary disabled placeholder col-6" aria-disabled="true"></a>
+</div>
+</div>
