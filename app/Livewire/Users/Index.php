6df578a5fa50b994b3e10d88a8bb183a85a922b6
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-11-14 08:11:29 +0700

    Create Livewire Users Index

---
+++ app/Livewire/Users/Index.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Livewire\Users;
+
+use App\Models\User;
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+        return view('livewire.users.index', [
+            'users' => User::query()->latest()->get()
+        ]);
+    }
+}
