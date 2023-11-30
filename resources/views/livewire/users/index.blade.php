06df5314b4cdb159c4bc6cc14a8f741c8f643f4d
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-11-30 14:20:23 +0700

    Update index

--- resources/views/livewire/users/index.blade.php
+++ resources/views/livewire/users/index.blade.php
@@ -1,3 +1,24 @@
 <div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">Joined</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($users as $user)
+                <tr wire:key="{{ $user->id }}">
+                    <th scope="row"> {{ $loop->index + 1 }} </th>
+                    <td> {{ $user->name }} </td>
+                    <td>{{ $user->email }}</td>
+                    <td>{{ $user->created_at->format('d F Y') }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+    {{ $users->links() }}
 </div>
